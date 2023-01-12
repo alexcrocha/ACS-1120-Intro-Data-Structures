3636f3712d31525b6c0a6c0359fbb2cb20f0bd29
Alex Rocha <rocha.amc@gmail.com>
2023-01-11 22:04:34 -0800

    Added dictionary_words module

---
+++ Code/dictionary_words.py
@@ -0,0 +1,20 @@
+import sys
+import random
+
+
+def generate_random_sentence(word_number):
+    """
+    Reads a list of words from dictionary and
+    returns a random sentence with the given number of words.
+    """
+
+    with open("/usr/share/dict/words") as f:
+        words = f.read().split()
+
+    random_words = random.choices(words, k=word_number)
+    return " ".join(random_words).capitalize() + "."
+
+
+if __name__ == "__main__":
+    number_of_words = int(sys.argv[1])
+    print(generate_random_sentence(number_of_words))
