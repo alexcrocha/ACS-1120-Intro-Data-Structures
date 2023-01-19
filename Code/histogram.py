fc9877de3830851b7761e24142718f7b0fe7483b
Alex Rocha <rocha.amc@gmail.com>
2023-01-18 17:42:55 -0800

    Complete histogram function

--- Code/histogram.py
+++ Code/histogram.py
@@ -4,8 +4,14 @@ sentence = "one fish two fish red fish blue fish"
 def histogram(source_text):
     """
     Takes a source_text argument (can be either a filename or the contents of the file as a string, your choice)
+    and returns a histogram of all words and the number of times they appear.
     """
-    pass
+    histogram = {}
+    list_of_words = source_text.split()
+    for word in list_of_words:
+        histogram[word] = list_of_words.count(word)
+    return histogram
+
 
 
 def unique_words(histogram):
@@ -26,3 +32,4 @@ def frequency(word, histogram):
 
 if __name__ == "__main__":
     print("hey")
+    print(histogram(sentence))
