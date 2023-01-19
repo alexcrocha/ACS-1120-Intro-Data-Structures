84fc25290a8e6084b893f0d5a489c9c114cba2e4
Alex Rocha <rocha.amc@gmail.com>
2023-01-18 17:29:11 -0800

    Create skeleton of functions

---
+++ Code/histogram.py
@@ -0,0 +1,28 @@
+sentence = "one fish two fish red fish blue fish"
+
+
+def histogram(source_text):
+    """
+    Takes a source_text argument (can be either a filename or the contents of the file as a string, your choice)
+    """
+    pass
+
+
+def unique_words(histogram):
+    """
+    That takes a histogram argument and returns the total count of unique words in the histogram.
+
+    """
+    pass
+
+
+def frequency(word, histogram):
+    """
+    Takes a word and histogram argument and returns the number of times that word appears in a text.
+
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print("hey")
