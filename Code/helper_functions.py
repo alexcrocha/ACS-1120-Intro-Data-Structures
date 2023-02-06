7110b1de403ebf55c8e636590c290b7bdf4b76df
Alex Rocha <rocha.amc@gmail.com>
2023-02-05 20:07:10 -0800

    Upgrade regex

--- Code/helper_functions.py
+++ Code/helper_functions.py
@@ -6,5 +6,5 @@ def read_file(file_name):
     Reads given source file and returns list of words
     """
     with open(file_name) as f:
-        words = re.findall(r"\w+", f.read().lower())
+        words = re.findall(r"\b\w+(?:[-']\w+)?\b", f.read().lower())
     return words
