6d77f3a329caaafd4fa123d9f39738a20f4cdb5b
Alex Rocha <rocha.amc@gmail.com>
2023-02-16 12:42:31 -0800

    Make processing faster

--- Code/sample_dict.py
+++ Code/sample_dict.py
@@ -1,12 +1,20 @@
 from dictogram2 import Dictogram
 from helper_functions import read_file
 
+words_histogram = {}
 
 def generate_markov(source_text, number=1):
     """
     Generates a random sentence from the given text and word count number
     """
-    histogram = Dictogram(source_text)
+
+    words_histogram["filled"] = (
+        Dictogram(source_text)
+        if len(words_histogram) == 0
+        else words_histogram["filled"]
+    )
+
+    histogram = words_histogram["filled"]
     word_list = []
     word_list.append(histogram.sample_start())
     for _ in range(number):
